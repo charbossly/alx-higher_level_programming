2fe96a094c9bf047029c7d02cdc25a2506caea48
CharbelAssogba <69874512+charbossly@users.noreply.github.com>
2023-06-17 15:40:16 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""
-This script lists all states from the
-database `hbtn_0e_0_usa`.
-"""
-
 import MySQLdb
 from sys import argv
-
 if __name__ == '__main__':
     """
     Access to the database and get the states
@@ -14,10 +8,8 @@ if __name__ == '__main__':
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
-
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
